c1a860c0fa70b690a18e38641219c94aea951211
Steven R. Loomis <srloomis@us.ibm.com>
2018-10-17 09:41:07 -0700

    tools, icu: actually failover if there are multiple URLs
    Building on #23269, if multiple ICU download URLs are present, try the
    next one in case of error.
    
    Part of the ICU 63.1 bump, but independent code-wise.
    https://github.com/nodejs/node/issues/23244
    
    PR-URL: https://github.com/nodejs/node/pull/23715
    Fixes: https://github.com/nodejs/node/issues/22344
    Reviewed-By: Refael Ackermann <refack@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>
    Reviewed-By: Gus Caplan <me@gus.host>
    Reviewed-By: Michael Dawson <michael_dawson@ca.ibm.com>
    (cherry picked from commit d8f2d2726143ecfbf99b90b7bbbc6f41b3cd95fc)

--- configure.py
+++ configure.py
@@ -1136,8 +1136,8 @@ def configure_intl(o):
         if (md5 == gotmd5):
           return targetfile
         else:
-          print('Expected: %s      *MISMATCH*' % md5)
-          print('\n ** Corrupted ZIP? Delete %s to retry download.\n' % targetfile)
+          warn('Expected: %s      *MISMATCH*' % md5)
+          warn('\n ** Corrupted ZIP? Delete %s to retry download.\n' % targetfile)
     return None
   icu_config = {
     'variables': {}
@@ -1284,11 +1284,12 @@ def configure_intl(o):
   # ICU source dir relative to tools/icu (for .gyp file)
   o['variables']['icu_path'] = icu_full_path
   if not os.path.isdir(icu_full_path):
-    print('* ECMA-402 (Intl) support didn\'t find ICU in %s..' % icu_full_path)
     # can we download (or find) a zipfile?
     localzip = icu_download(icu_full_path)
     if localzip:
       nodedownload.unpack(localzip, icu_parent_path)
+    else:
+      warn('* ECMA-402 (Intl) support didn\'t find ICU in %s..' % icu_full_path)
   if not os.path.isdir(icu_full_path):
     print('Cannot build Intl without ICU in %s.' % icu_full_path)
     print('(Fix, or disable with "--with-intl=none" )')

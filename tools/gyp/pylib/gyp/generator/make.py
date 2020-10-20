8de6304f4346aebc6467f8e7c250b62cf16d6792
Su Baocheng <baocheng.su@siemens.com>
2020-10-20 15:57:35 +0800

    py2to3: fix tools/gyp/pylib/gyp/generator/make.py
    Signed-off-by: Su Baocheng <baocheng.su@siemens.com>

--- tools/gyp/pylib/gyp/generator/make.py
+++ tools/gyp/pylib/gyp/generator/make.py
@@ -1753,8 +1753,8 @@ $(obj).$(TOOLSET)/$(TARGET)/%%.o: $(obj)/%%%s FORCE_DO_CMD
       # - The multi-output rule will have an do-nothing recipe.
 
       # Hash the target name to avoid generating overlong filenames.
-      cmddigest = hashlib.sha1(command if command else self.target).hexdigest()
-      intermediate = "%s.intermediate" % (cmddigest)
+      cmddigest = hashlib.sha1((command or self.target).encode("utf-8")).hexdigest()
+      intermediate = "%s.intermediate" % cmddigest
       self.WriteLn('%s: %s' % (' '.join(outputs), intermediate))
       self.WriteLn('\t%s' % '@:');
       self.WriteLn('%s: %s' % ('.INTERMEDIATE', intermediate))
